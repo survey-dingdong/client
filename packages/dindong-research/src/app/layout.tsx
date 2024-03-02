d1ec4d39ef626c544f0feb73f5960fd5ba576c28
Daeun Yang <daeun.yang@elicer.com>
2024-03-02 13:54:39 +0900

    chore: set initial settings about msw with bugs

--- packages/dindong-research/src/app/layout.tsx
+++ packages/dindong-research/src/app/layout.tsx
@@ -1,13 +1,27 @@
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "../theme";
+import React from "react";
+import { worker } from "../mocks/browser";
+import { MSWComponent } from "./_component/MSWComponent";
 
 export default function RootLayout(props: any) {
   const { children } = props;
+
+  React.useEffect(() => {
+    // Start the mocking when the application starts.
+    worker.start();
+    // Clean up once the component is unmounted.
+    return () => {
+      worker.stop();
+    };
+  }, []);
+
   return (
     <html lang="en">
       <AppRouterCacheProvider options={{ enableCssLayer: true }}>
         <ThemeProvider theme={theme}>
+          <MSWComponent />
           <body style={{ margin: 0, backgroundColor: "#F8FAFB" }}>
             {children}
           </body>
