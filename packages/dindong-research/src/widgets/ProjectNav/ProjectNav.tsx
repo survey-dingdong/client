517c82eadf6683bbd367b5267431f3d3c518e39e
Daeun Yang <daeun.yang@elicer.com>
2024-05-18 10:46:11 +0900

    feature: delete project

--- packages/dindong-research/src/widgets/ProjectNav/ProjectNav.tsx
+++ packages/dindong-research/src/widgets/ProjectNav/ProjectNav.tsx
@@ -2,6 +2,11 @@
 
 import {
   Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
   Drawer,
   List,
   ListItem,
@@ -13,11 +18,17 @@ import {
 import React from "react";
 
 import Link from "next/link";
-import { useParams, usePathname } from "next/navigation";
+import { useParams, usePathname, useRouter } from "next/navigation";
 import Nav from "src/shared/Nav";
 import ArrowBackRoundedIcon from "@mui/icons-material/ArrowBackRounded";
 import { usePath } from "src/hooks/usePath";
 import { useWorkspaces } from "src/hooks/useWorkspaces";
+import axios from "axios";
+import { ProjectTypeEnum } from "generated-client";
+import { useSnackbar } from "notistack";
+import { useQueryClient } from "@tanstack/react-query";
+import { getProjectsQueryKey } from "src/hooks/useProjects";
+import { useProject } from "src/hooks/useProject";
 //
 //
 //
@@ -29,13 +40,76 @@ export const drawerWidth = 256;
 //
 
 const ProjectNav = () => {
+  const router = useRouter();
+  const queryClient = useQueryClient();
+  const { enqueueSnackbar } = useSnackbar();
+
+  const { projectId, workspaceId } = useParams();
+  const _workspaceId = Number(workspaceId);
+
   const { workspaces } = useWorkspaces();
+  const { project } = useProject({
+    workspaceId: Number(workspaceId),
+    projectId: Number(projectId),
+  });
 
   const informationPath = usePath({ type: "project", slug: "/information" });
   const participantsPath = usePath({ type: "project", slug: "/participants" });
   const chatPath = usePath({ type: "project", slug: "/chat" });
 
+  const workspacesPath = usePath({
+    type: "workspace",
+    slug: "/",
+    workspaceId: _workspaceId,
+  });
+
   const pathname = usePathname();
+
+  const [open, setOpen] = React.useState(false);
+
+  /**
+   *
+   */
+  const handleDeleteProject = async () => {
+    try {
+      await axios.delete(`/workspaces/${workspaceId}/projects/${projectId}`, {
+        params: { project_type: ProjectTypeEnum.Experiment },
+      });
+
+      queryClient.invalidateQueries({ queryKey: getProjectsQueryKey });
+
+      router.push(workspacesPath);
+
+      enqueueSnackbar("프로젝트가 삭제되었습니다.", { variant: "success" });
+    } catch (_) {
+      enqueueSnackbar("프로젝트 삭제에 실패했습니다.", { variant: "error" });
+    }
+  };
+
+  const handleDialogClose = () => {
+    setOpen(false);
+  };
+
+  const renderDialog = () => {
+    return (
+      <Dialog open={open} maxWidth="xs" fullWidth onClose={handleDialogClose}>
+        <DialogTitle>프로젝트 삭제</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            <b>{project?.title}</b> 프로젝트가 삭제됩니다. 계속하시겠습니까?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button color="inherit" onClick={handleDialogClose}>
+            취소
+          </Button>
+          <Button color="error" onClick={handleDeleteProject}>
+            삭제
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+  };
 
   //
   //
@@ -119,7 +193,10 @@ const ProjectNav = () => {
       </List>
 
       {/* add workspace button */}
-      <Button color="inherit">프로젝트 삭제</Button>
+      <Button color="inherit" onClick={() => setOpen(true)}>
+        프로젝트 삭제
+      </Button>
+      {renderDialog()}
     </Nav>
   );
 };
