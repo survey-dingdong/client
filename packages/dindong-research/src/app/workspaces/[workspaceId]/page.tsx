57e0b6d80d8e9b175a7d144fa34afe54561d48bb
Daeun Yang <daeun.yang@elicer.com>
2024-07-07 18:07:01 +0900

    feat: add LOADING SPINNER in project list

--- packages/dindong-research/src/app/workspaces/[workspaceId]/page.tsx
+++ packages/dindong-research/src/app/workspaces/[workspaceId]/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Container, Stack } from "@mui/material";
+import { Box, CircularProgress, Container, Stack } from "@mui/material";
 import { useProjects } from "src/hooks/useProjects";
 import ProjectList from "src/widgets/ProjectList/ProjectList";
 import { useParams } from "next/navigation";
@@ -14,19 +14,27 @@ export default function Page() {
   const params = useParams<{ workspaceId: string }>();
   const _workspaceId = Number(params?.workspaceId);
 
-  const { projects } = useProjects({
+  const { projects, isLoading } = useProjects({
     workspaceId: _workspaceId,
   });
-
-  const hasNoProjects = projects?.length === 0;
 
   return (
     <ContentContainer>
       <Stack height="100%">
         <SurveyListPageHeader />
-
-        <ProjectList projects={projects} workspaceId={_workspaceId} />
-        {hasNoProjects ? <SurveyListEmpty workspaceId={_workspaceId} /> : null}
+        {isLoading ? (
+          <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            height="100%"
+            width="100%"
+          >
+            <CircularProgress />
+          </Box>
+        ) : (
+          <ProjectList projects={projects} workspaceId={_workspaceId} />
+        )}
       </Stack>
     </ContentContainer>
   );
