934a2d96f6beb9dd811d45ba2a06d5355ae9aabe
Daeun Yang <daeun.yang@elicer.com>
2024-07-14 17:08:37 +0900

    add DISABLED condition tp CREATE PROJECT BUTTON

--- packages/dindong-research/src/widgets/CreateProjectAction/CreateProjectAction.tsx
+++ packages/dindong-research/src/widgets/CreateProjectAction/CreateProjectAction.tsx
@@ -6,6 +6,7 @@ import {
   DialogActions,
   DialogContent,
   DialogTitle,
+  Tooltip,
 } from "@mui/material";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 import { TextField } from "src/shared";
@@ -33,6 +34,18 @@ const CreateProjectDialog: React.FC<CreateProjectDialogProps> = ({
   const queryClient = useQueryClient();
   const [value, setValue] = React.useState("");
 
+  const noValue = !value.length;
+
+  React.useEffect(() => {
+    if (open) {
+      setValue("");
+    }
+  }, [open]);
+
+  //
+  //
+  //
+
   return (
     <Dialog open={open} maxWidth="xs" fullWidth onClose={onClose}>
       <DialogTitle>실험 프로젝트 만들기</DialogTitle>
@@ -40,6 +53,7 @@ const CreateProjectDialog: React.FC<CreateProjectDialogProps> = ({
         <TextField
           required
           fullWidth
+          value={value}
           label="프로젝트 명"
           placeholder="프로젝트 명을 입력해주세요"
           onChange={(e) => setValue(e.target.value)}
@@ -51,37 +65,42 @@ const CreateProjectDialog: React.FC<CreateProjectDialogProps> = ({
         <Button color="inherit" onClick={onClose}>
           취소
         </Button>
-        <Button
-          onClick={() =>
-            createProjectWorkspacesWorkspaceIdProjectsPost({
-              workspaceId,
-              projectType: "experiment",
-              requestBody: {
-                title: value,
-              },
-            })
-              .then(() => {
-                enqueueSnackbar("프로젝트가 만들어졌습니다.", {
-                  variant: "success",
-                });
+        <Tooltip title={noValue ? "필수 항목을 입력해 주세요." : ""}>
+          <span>
+            <Button
+              disabled={noValue}
+              onClick={() =>
+                createProjectWorkspacesWorkspaceIdProjectsPost({
+                  workspaceId,
+                  projectType: "experiment",
+                  requestBody: {
+                    title: value,
+                  },
+                })
+                  .then(() => {
+                    enqueueSnackbar("프로젝트가 만들어졌습니다.", {
+                      variant: "success",
+                    });
 
-                queryClient.invalidateQueries({
-                  queryKey: getProjectsQueryKey,
-                });
-                onClose();
-              })
-              .catch(() => {
-                enqueueSnackbar(
-                  "에러가 발생했습니다. 새로고침 후 다시 시도해주세요.",
-                  {
-                    variant: "error",
-                  }
-                );
-              })
-          }
-        >
-          만들기
-        </Button>
+                    queryClient.invalidateQueries({
+                      queryKey: getProjectsQueryKey,
+                    });
+                    onClose();
+                  })
+                  .catch(() => {
+                    enqueueSnackbar(
+                      "에러가 발생했습니다. 새로고침 후 다시 시도해주세요.",
+                      {
+                        variant: "error",
+                      }
+                    );
+                  })
+              }
+            >
+              만들기
+            </Button>
+          </span>
+        </Tooltip>
       </DialogActions>
     </Dialog>
   );
