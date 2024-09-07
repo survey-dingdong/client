065fe6e006ac87c860583c1d97f8d879b398d3a9
Daeun Yang <daeun.yang@elicer.com>
2024-09-07 13:53:58 +0900

    feat: add Icon component and set build env

--- packages/design-system/src/main.tsx
+++ packages/design-system/src/main.tsx
@@ -1,14 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { ThemeProvider } from "@mui/material";
-import { theme } from "./constants/theme.ts";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
-  </StrictMode>
-);
+createRoot(document.getElementById("root")!).render(<></>);
--- packages/design-system/vite.config.ts
+++ packages/design-system/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,4 +11,12 @@ export default defineConfig({
       include: "**/*.svg?react",
     }),
   ],
+  build: {
+    outDir: "dist",
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "DesignSystem",
+      fileName: (format) => `design-system.${format}.js`,
+    },
+  },
 });
