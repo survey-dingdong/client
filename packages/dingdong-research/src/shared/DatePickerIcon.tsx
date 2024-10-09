837a4c940c32d319e0675468cc3259934be87ea9
Daeun Yang <daeun.yang@elicer.com>
2024-10-09 21:24:21 +0900

    feat: move theme and shared component to ds

--- packages/dingdong-research/src/shared/DatePickerIcon.tsx
+++ packages/dingdong-research/src/shared/DatePickerIcon.tsx
@@ -1,4 +1,4 @@
-import Icon from "@dingdong/design-system/src/components/icon/Icon";
+import { Icon } from "@dingdong/design-system/src";
 import React from "react";
 
 export function DatePickerOpenIcon() {
