d74dee687d4d9c6738de8f83fa785195c6d83d52
Daeun Yang <daeun.yang@elicer.com>
2024-11-14 01:25:13 +0900

    feat: implement bottom navigation and extend theme types

--- apps/dingdong/src/components/layout/BottomNav.tsx
+++ apps/dingdong/src/components/layout/BottomNav.tsx
@@ -1,8 +1,30 @@
-import { AppBar } from "@mui/material";
+import { BottomNavigation, BottomNavigationAction, Paper } from "@mui/material";
+import { Icon } from "dingdong-ds";
 import React from "react";
+import { BOTTOM_NAVS } from "./_constants/bottomNav";
 
 const BottomNav: React.FC = () => {
-  return <AppBar>bottom</AppBar>;
+  const [selectedNav, setSelectedNav] = React.useState(0);
+
+  return (
+    <Paper sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}>
+      <BottomNavigation
+        showLabels
+        value={selectedNav}
+        onChange={(_, newValue) => {
+          setSelectedNav(newValue);
+        }}
+      >
+        {BOTTOM_NAVS.map((nav, index) => (
+          <BottomNavigationAction
+            key={index}
+            label={nav.label}
+            icon={<Icon icon="edit" />}
+          />
+        ))}
+      </BottomNavigation>
+    </Paper>
+  );
 };
 
 export default BottomNav;
---
+++ apps/dingdong/src/components/layout/_constants/bottomNav.ts
@@ -0,0 +1,17 @@
+export const BOTTOM_NAVS = [
+  {
+    label: "실험 공고",
+  },
+  {
+    label: "참여 관리",
+  },
+  {
+    label: "채팅",
+  },
+  {
+    label: "알림",
+  },
+  {
+    label: "My",
+  },
+];
