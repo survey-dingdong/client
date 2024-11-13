d6a79cdac0cc3cd2beb1c09fc168e3f8add4af9a
Daeun Yang <daeun.yang@elicer.com>
2024-11-13 22:15:46 +0900

    feat: set PWA initial setting

---
+++ packages/dingdong/src/components/layout/BottomNav.tsx
@@ -0,0 +1,8 @@
+import { AppBar } from "@mui/material";
+import React from "react";
+
+const BottomNav: React.FC = () => {
+  return <AppBar>bottom</AppBar>;
+};
+
+export default BottomNav;
--- packages/dingdong/vite.config.ts
+++ packages/dingdong/vite.config.ts
@@ -1,49 +1,36 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      includeAssets: [
-        "favicon.svg",
-        "favicon.ico",
-        "robots.txt",
-        "apple-touch-icon.png",
-      ],
-      manifest: {
-        name: "Dingdong",
-        short_name: "dingdong",
-        description: "for research and survey",
-        theme_color: "#ffffff",
-        icons: [
-          {
-            src: "favicon-64x64.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-        ],
-        // icons: [
-        //   {
-        //     src: 'pwa-192x192.png',
-        //     sizes: '192x192',
-        //     type: 'image/png'
-        //   },
-        //   {
-        //     src: 'pwa-512x512.png',
-        //     sizes: '512x512',
-        //     type: 'image/png'
-        //   },
-        //   {
-        //     src: 'pwa-512x512.png',
-        //     sizes: '512x512',
-        //     type: 'image/png',
-        //     purpose: 'any maskable'
-        //   }
-        // ]
-      },
-    }),
-  ],
-});
+  plugins: [react(), VitePWA({
+    registerType: 'prompt',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'dingond-user',
+      short_name: 'dingond-user',
+      description: 'description',
+      theme_color: '#ffffff',
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+
+    devOptions: {
+      enabled: false,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
+})
