7cf48dfbc47836c4a61949e8bb3736044a240c4c
dana-y <75559282+dana-y@users.noreply.github.com>
2025-03-17 15:51:04 +0000

    Deploying to gh-pages from @ survey-dingdong/client@0a78033faf28d0d91dc75c37861c4acb7a4f9bde 🚀

--- assets/Icon.stories-_Rr4WCDc.js
+++
@@ -1,7 +0,0 @@
-import{I as t,a}from"./Tag-ijJ3Q2FL.js";import"./jsx-runtime-Cw0GR0a5.js";import"./index-CTjT7uj6.js";import"./theme-BCdS4ir7.js";import"./DefaultPropsProvider-CyOCPYDc.js";import"./Box-C7cojReu.js";import"./createSimplePaletteValueFilter-bm0fmN_7.js";const y={title:"Icon/Icon",component:t,parameters:{layout:"centered"},argTypes:{color:{options:["primary","secondary","info","error","warning","success"],control:{type:"radio"}},size:{options:["small","medium","large"],control:{type:"radio"}},icon:{options:Object.keys(a),control:{type:"select"}}}},r={args:{icon:"userSearch",color:"primary",size:"medium"}};var o,e,s;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {
-    icon: "userSearch",
-    color: "primary",
-    size: "medium"
-  }
-}`,...(s=(e=r.parameters)==null?void 0:e.docs)==null?void 0:s.source}}};const u=["UserSearchIconStory"];export{r as UserSearchIconStory,u as __namedExportsOrder,y as default};
