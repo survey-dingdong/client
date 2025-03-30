054deeba7938d215e12fd15a94f19f5df84f73ac
daeun.Yang <75559282+dana-y@users.noreply.github.com>
2025-03-30 16:29:13 +0900

    chore: change build file path

--- apps/dingdong/vite.config.ts
+++ apps/dingdong/vite.config.ts
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "dist/client",
+  },
   define: {
     "process.platform": JSON.stringify(process.platform || "linux"),
   },
